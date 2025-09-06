c192e5d3c54e3dd6d39eb71f18135418db8f2947
kacpeuuuu <bielawa.kacper6@gmail.com>
2025-09-06 20:03:02 +0200

    changed the code so that it does anything

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from PIL import Image
-import os, sys
 
-for infile in sys.argv[1:]:
-    f, e = os.path.splitext(infile)
-    outfile = f + ".jpg"
-    if infile != outfile:
-        try:
-            with Image.open(infile) as im:
-                im.save(outfile)
-        except OSError:
-            print("cannot convert", infile)
+im = Image.open("image-to-grayscale/dog.jpg")
+width, height = im.size
+
+im = im.convert("RGB")
+
+new_img = Image.new("RGB", (width, height))
+src_pixels = im.load()
+dst_pixels = new_img.load()
+
+for x in range(width):
+    for y in range(height):
+        rgb_val = src_pixels[x,y]
+        g = int(rgb_val[0]*1/3+rgb_val[1]*1/3+rgb_val[2]*1/3)
+        dst_pixels[x,y] = (g,g,g)
+
+new_img.save('grayscaled_picture.jpg')
